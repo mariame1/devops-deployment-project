94425f0e6820afbe8e6c61cee92830814c3869d9
Ubuntu <ubuntu@ip-172-31-17-122.ec2.internal>
2025-06-23 19:20:23 +0000

    comment deleted in deploy_to_ec2.sh

--- scripts/deploy_to_ec2.sh
+++ scripts/deploy_to_ec2.sh
@@ -2,7 +2,7 @@
 
 EC2_USER="ubuntu"
 EC2_IP="34.234.74.38"
-KEY_PATH="$HOME/.ssh/app-deploy-key.pem"  # or: ~/.ssh/app-deploy-key.pem
+KEY_PATH="$HOME/.ssh/app-deploy-key.pem"  
 
 echo "[Deploying to EC2 at $EC2_IP]"
 
